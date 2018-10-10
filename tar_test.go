f92d48bbcfb30e374920e86cfcda798793717ff0
Alex Nichol <unixpickle@gmail.com>
2018-10-10 19:43:23 -0400

    test tar prefixes

--- tar_test.go
+++ tar_test.go
@@ -30,50 +30,59 @@ func TestTar(t *testing.T) {
 	essentials.Must(ioutil.WriteFile(filepath.Join(dir, longFilename, longFilename+longFilename),
 		[]byte(longFilename+longFilename+longFilename+longFilename), 0664))
 
-	tarObj, err := Tar(dir, "")
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, prefix := range []string{"", "Dir"} {
+		t.Run("Prefix"+prefix, func(t *testing.T) {
+			tarObj, err := Tar(dir, prefix)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	tarFile, err := tarObj.Open()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer tarFile.Close()
+			tarFile, err := tarObj.Open()
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer tarFile.Close()
 
-	tarReader := tar.NewReader(tarFile)
+			tarReader := tar.NewReader(tarFile)
 
-	entries := []struct {
-		Name     string
-		Contents string
-	}{
-		{longFilename, ""},
-		{longFilename + "/" + longFilename, "this is a test"},
-		{
-			longFilename + "/" + longFilename + longFilename,
-			longFilename + longFilename + longFilename + longFilename,
-		},
-		{"file1", "testing"},
-		{"file2", "toasting123"},
-	}
+			entries := []struct {
+				Name     string
+				Contents string
+			}{
+				{"", ""},
+				{longFilename, ""},
+				{longFilename + "/" + longFilename, "this is a test"},
+				{
+					longFilename + "/" + longFilename + longFilename,
+					longFilename + longFilename + longFilename + longFilename,
+				},
+				{"file1", "testing"},
+				{"file2", "toasting123"},
+			}
+			if prefix == "" {
+				entries = entries[1:]
+			}
 
-	for i, entry := range entries {
-		header, err := tarReader.Next()
-		if err != nil {
-			t.Fatalf("file %d: error %s", i, err)
-		}
-		if header.Name != entry.Name {
-			t.Errorf("expected name %s but got %s", entry.Name, header.Name)
-		}
-		data, err := ioutil.ReadAll(tarReader)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if string(data) != entry.Contents {
-			t.Errorf("expected contents %s but got %s", entry.Contents, string(data))
-		}
-	}
-	if _, err := tarReader.Next(); err != io.EOF {
-		t.Error("expected EOF")
+			for i, entry := range entries {
+				header, err := tarReader.Next()
+				if err != nil {
+					t.Fatalf("file %d: error %s", i, err)
+				}
+				entry.Name = filepath.Join(prefix, entry.Name)
+				if header.Name != entry.Name {
+					t.Errorf("expected name %s but got %s", entry.Name, header.Name)
+				}
+				data, err := ioutil.ReadAll(tarReader)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if string(data) != entry.Contents {
+					t.Errorf("expected contents %s but got %s", entry.Contents, string(data))
+				}
+			}
+			if _, err := tarReader.Next(); err != io.EOF {
+				t.Error("expected EOF")
+			}
+		})
 	}
 }

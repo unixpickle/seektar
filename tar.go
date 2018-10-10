0a8b518a4d2b9d8e6ca6c27835438608c754b641
Alex Nichol <unixpickle@gmail.com>
2018-10-10 18:36:23 -0400

    drafted tar encoding

---
+++ tar.go
@@ -0,0 +1,122 @@
+package seektar
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/unixpickle/essentials"
+)
+
+type TarTypeFlag byte
+
+const (
+	NormalFile TarTypeFlag = 0
+	Directory  TarTypeFlag = 5
+)
+
+// Tar generates a tarball as a Piece.
+//
+// The result is deterministic, provided that the contents
+// of the directory do not change.
+func Tar(dirPath, prefix string) (Agg, error) {
+	var pieces []Piece
+
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		relName, err := filepath.Rel(dirPath, path)
+		if err != nil {
+			return err
+		}
+		header := &tarHeader{
+			Filename: filepath.Join(prefix, relName),
+			FileMode: uint(info.Mode() & os.ModePerm),
+			ModTime:  uint64(info.ModTime().Unix()),
+		}
+		if info.IsDir() {
+			header.Type = Directory
+		} else {
+			header.FileSize = uint64(info.Size())
+			header.Type = NormalFile
+		}
+		// TODO: get owner information.
+		pieces = append(pieces, BytePiece(header.Encode()))
+		if !info.IsDir() {
+			piece, err := NewFilePiece(path)
+			if err != nil {
+				return err
+			}
+			pieces = append(pieces, piece)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, essentials.AddCtx("tar directory", err)
+	}
+	return Agg(pieces), nil
+}
+
+type tarHeader struct {
+	Filename    string
+	FileMode    uint
+	OwnerID     uint
+	GroupID     uint
+	FileSize    uint64
+	ModTime     uint64
+	Type        TarTypeFlag
+	LinkedFile  string
+	OwnerName   string
+	GroupName   string
+	DeviceMajor uint
+	DeviceMinor uint
+}
+
+func (t *tarHeader) Encode() []byte {
+	var res bytes.Buffer
+	filenameEnd := []byte(t.Filename)
+	filenameStart := []byte{}
+	if len(filenameEnd) > 100 {
+		suffixSize := essentials.MaxInt(0, len(filenameEnd)-155)
+		filenameStart = filenameEnd[:suffixSize]
+		filenameEnd = filenameEnd[suffixSize:]
+	}
+	padNull(&res, filenameStart, 100)
+	res.WriteString(fmt.Sprintf("%06o \x00", t.FileMode))
+	res.WriteString(fmt.Sprintf("%06o \x00", t.OwnerID))
+	res.WriteString(fmt.Sprintf("%06o \x00", t.GroupID))
+	res.WriteString(fmt.Sprintf("%11o\x00", t.FileSize))
+	res.WriteString(fmt.Sprintf("%11o\x00", t.ModTime))
+	res.WriteString("        ")
+	res.WriteByte(byte(t.Type))
+	padNull(&res, []byte(t.LinkedFile), 100)
+	res.WriteString(fmt.Sprintf("ustar\x0000"))
+	padNull(&res, []byte(t.OwnerName), 32)
+	padNull(&res, []byte(t.GroupName), 32)
+	res.WriteString(fmt.Sprintf("%06o \x00", t.DeviceMajor))
+	res.WriteString(fmt.Sprintf("%06o \x00", t.DeviceMinor))
+	padNull(&res, filenameStart, 155)
+	for res.Len() < 512 {
+		res.WriteByte(0)
+	}
+
+	resBytes := res.Bytes()
+	var sum uint
+	for _, b := range resBytes {
+		sum += uint(b)
+	}
+	copy(resBytes[148:], []byte(fmt.Sprintf("%06o\x00 ", sum)))
+	return resBytes
+}
+
+func padNull(out *bytes.Buffer, data []byte, length int) {
+	if len(data) > length {
+		out.Write(data[:length])
+	} else if len(data) == length {
+		out.Write(data)
+	} else {
+		out.Write(data)
+		for i := len(data); i < length; i++ {
+			out.WriteByte(0)
+		}
+	}
+}
